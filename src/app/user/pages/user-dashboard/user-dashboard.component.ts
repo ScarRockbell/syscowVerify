fa004b7deae13b6cc948618459a10cff27f38c0c
KissKazuelas <mrjorge221@gmail.com>
2021-12-03 13:37:03 -0600

    feat: primer grafica

--- src/app/user/pages/user-dashboard/user-dashboard.component.ts
+++ src/app/user/pages/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EdadGanado } from '../../interfaces/reportes-interfaces';
+import { ReportesService } from '../../services/reportes.service';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,9 +9,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+  reporteEdadGanado?:EdadGanado[];
+  basicOptions : any;
+  dataGraficaEdadPorAnimal : any;
+
+  columnasEdadPorAnima: any;
+  filasEdadPorAnima: any;
+
+  constructor(
+    private repService : ReportesService
+  ) { }
 
   ngOnInit(): void {
+    this.edadPorAnimal();
+    this.basicOptions = {
+      plugins: {
+          legend: {
+              labels: {
+                  color: 'black'
+              }
+          }
+      },
+      scales: {
+          x: {
+              ticks: {
+                  color: 'black'
+              },
+              grid: {
+                  color: 'rgba(255,255,255,0.2)'
+              }
+          },
+          y: {
+              ticks: {
+                  color: 'black'
+              },
+              grid: {
+                  color: 'rgba(255,255,255,0.2)'
+              }
+          }
+      }
+  };
+  }
+
+
+  edadPorAnimal(){
+    const value: string | null = localStorage.getItem('jwt');
+    this.repService.getEdadGanado((value ? value : ' '))
+    .subscribe(resp=>{
+      this.reporteEdadGanado=resp.result;
+      this.columnasEdadPorAnima = this.reporteEdadGanado?.map(animal=> {return animal.Anios} );
+      this.filasEdadPorAnima = this.reporteEdadGanado?.map(animal=> {return animal.Total} );
+      this.configurarEdadPorAnimal();
+    });
+  }
+
+  configurarEdadPorAnimal(){
+    this.dataGraficaEdadPorAnimal = {
+      labels: this.columnasEdadPorAnima,
+      datasets: [
+          {
+              label: 'Años',
+              backgroundColor: '#42A5F5',
+              data: this.filasEdadPorAnima
+          }
+      ]
+  };
   }
 
 }
